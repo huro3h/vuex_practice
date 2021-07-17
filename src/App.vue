f59adb88ad3b9259eca7b5ce334da529f98384f5
huro3h <huro3h@gmail.com>
2021-07-18 02:43:54 +0900

    define tasks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    TODO:
+  <div id="app">
+    <h4>タスク一覧</h4>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:cheched="task.done">
+        {{ task.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App.vue"
+  computed: {
+    tasks () {
+      return this.$store.state.tasks
+    },
+  },
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
-// ストアを定義
+// store.js
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
 const store = new Vuex.Store({
-  // TODO
+  state: {
+    // タスクの初期ステート
+    tasks: [
+      { id: 1, name: '食う', done: false },
+      { id: 2, name: '寝る', done: false },
+      { id: 3, name: 'あそぶ', done: false },
+    ]
+  }
 })
 
 // 定義したストアをエクスポート
