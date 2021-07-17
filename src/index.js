7b2dbc9a1b13b01c6c053338409f9eb629fee921
huro3h <huro3h@gmail.com>
2021-07-17 11:56:37 +0900

    store.commit 'increment'

--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@ const store = new Vuex.Store({
   // ステート
   state: {
     count: 10
+  },
+  // ミューテーション
+  mutations: {
+    increment (state, amount) {
+      state.count += amount
+    }
   }
 })
+
+console.log(store.state.count);
+
+store.commit('increment', 45)
 
 console.log(store.state.count);
