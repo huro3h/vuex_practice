9dc21eb55de9810f1d04d549b63685f897b55987
huro3h <huro3h@gmail.com>
2021-07-17 15:16:51 +0900

    add App.vue,  store.js

--- src/index.js
+++ src/index.js
@@ -1,63 +1,66 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import App from './App'
+import store from './store'
 
-Vue.use(Vuex)
 
-function asyncGetCountNum(type) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      let amount = 10
-      switch (type) {
-        case 'one':
-          amount
-          break
-        case 'two':
-          amount = amount * 2
-          break
-        default:
-          amount = amount * 100
-      }
-      resolve({amount})
-    }, 2000)
-  })
-}
-
-const store = new Vuex.Store({
-  // ステート(初期値)
-  state: {
-    count: 10
-  },
-  // ゲッター
-  getters: {
-    double: (state) => state.count * 2,
-    triple: (state) => state.count * 3,
-    multiple: (state, getters) => getters.double * getters.triple
-  },
-  // ミューテーション
-  mutations: {
-    increment(state, payload) {
-      state.count += payload.amount
-    }
-  },
-  // アクション
-  actions: {
-    incrementAsync({commit}, payload) {
-      return asyncGetCountNum(payload.type)
-        .then(data => {
-          console.log(data);
-          
-          commit('increment', {
-            amount: data.amount
-          })
-        })
-    }
-    // incrementAction (ctx) {
-    //   ctx.commit('increment') // incrementミューテーションの実行
-    // }
-  }
-})
-
-console.log(store.state.count);
-store.dispatch('incrementAsync', { type: 'two' }).then(() => {
-  console.log(store.state.count);
-})
+// Vue.use(Vuex)
+//
+// function asyncGetCountNum(type) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       let amount = 10
+//       switch (type) {
+//         case 'one':
+//           amount
+//           break
+//         case 'two':
+//           amount = amount * 2
+//           break
+//         default:
+//           amount = amount * 100
+//       }
+//       resolve({amount})
+//     }, 2000)
+//   })
+// }
+//
+// const store = new Vuex.Store({
+//   // ステート(初期値)
+//   state: {
+//     count: 10
+//   },
+//   // ゲッター
+//   getters: {
+//     double: (state) => state.count * 2,
+//     triple: (state) => state.count * 3,
+//     multiple: (state, getters) => getters.double * getters.triple
+//   },
+//   // ミューテーション
+//   mutations: {
+//     increment(state, payload) {
+//       state.count += payload.amount
+//     }
+//   },
+//   // アクション
+//   actions: {
+//     incrementAsync({commit}, payload) {
+//       return asyncGetCountNum(payload.type)
+//         .then(data => {
+//           console.log(data);
+//
+//           commit('increment', {
+//             amount: data.amount
+//           })
+//         })
+//     }
+//     // incrementAction (ctx) {
+//     //   ctx.commit('increment') // incrementミューテーションの実行
+//     // }
+//   }
+// })
+//
+// console.log(store.state.count);
+// store.dispatch('incrementAsync', { type: 'two' }).then(() => {
+//   console.log(store.state.count);
+// })
---
+++ src/store.js
@@ -0,0 +1,7 @@
+// ストアを定義
+const store = new Vuex.Store({
+  // TODO
+})
+
+// 定義したストアをエクスポート
+export default store
