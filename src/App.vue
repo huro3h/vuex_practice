80e3f91867529768f7ffb341fb898eb3d08a00ed
huro3h <huro3h@gmail.com>
2021-07-19 19:38:44 +0900

    list items components

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,47 @@
     <h4>タスク一覧</h4>
     <ul>
       <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:cheched="task.done">
+        <input type="checkbox" v-bind:cheched="task.done"
+          v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
       </li>
     </ul>
+
+    <form v-on:submit="toggleTaskStatus(task)">
+      <input type="text" v-model="newTaskName" placeholder="新しいタスク名">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      newTaskName: '',
+    }
+  },
   computed: {
     tasks () {
       return this.$store.state.tasks
     },
   },
+  methods: {
+    // タスクを追加する
+    addTask () {
+      // addTaskミューテーションをコミット
+      this.$store.commit('addTask', {
+        name: this.newTaskName,
+      })
+      this.newTaskName = ''
+    },
+
+    // タスクの完了状態を更新する
+    toggleTaskStatus (task) {
+      // toggleTaskStatusミューテーションをコミット
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
+    },
+  }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -12,7 +12,31 @@ const store = new Vuex.Store({
       { id: 2, name: '寝る', done: false },
       { id: 3, name: 'あそぶ', done: false },
     ]
-  }
+  },
+  nextTaskId: 4,
+  
+  mutations: {
+    addTask (state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      })
+      
+      state.nextTaskId++
+    },
+    
+    // タスクの完了状態を変更する
+    toggleTaskStatus (state, { id }) {
+      const filterd = state.tasks.filter(task => {
+        return task.id === id
+      })
+  
+      filterd.forEach(task => {
+        task.done = !task.done // ここで値を反転
+      })
+    },
+  },
 })
 
 // 定義したストアをエクスポート
