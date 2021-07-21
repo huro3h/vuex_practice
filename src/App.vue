7fb7b9eda149a73a70013d13fda7df5877dccc4e
huro3h <huro3h@gmail.com>
2021-07-21 22:51:01 +0900

    add label list and update functions

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,29 @@
     <h4>タスク一覧</h4>
     <ul>
       <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:cheched="task.done"
-          v-on:change="toggleTaskStatus(task)">
+        <input type="checkbox" v-bind:cheched="task.done" v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
+        -
+        <span v-for="id in task.labelIds" v-bind:key="id">
+          {{ getLabelText(id) }}
+        </span>
       </li>
     </ul>
 
     <form v-on:submit.prevent="addTask">
       <input type="text" v-model="newTaskName" placeholder="新しいタスク名">
+    </form>
+
+    <h3>ラベル一覧</h3>
+    <ul>
+      <li v-for="label in labels" v-bind:key="label.id">
+        <input type="checkbox" v-bind:value="label.id" v-model="newTaskLabelIds">
+        {{ label.text }}
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="addLabel">
+      <input type="text" v-model="newLabelText" placeholder="新しいラベル名を入れてね">
     </form>
   </div>
 </template>
@@ -19,20 +34,34 @@
 export default {
   data () {
     return {
+      // 入力中の新しいタスク名を一時的に保持する
       newTaskName: '',
+
+      // 新しいタスクに紐づくラベル一覧を一時的に保持する
+      newTaskLabelIds: [],
+
+      // 入力中の新しいラベル名を一時的に保持する
+      newLabelText: ''
     }
   },
   computed: {
     tasks () {
       return this.$store.state.tasks
     },
+    labels () {
+      return this.$store.state.labels
+    }
   },
+
   methods: {
     addTask () {
+      // addTask ミューテーションをコミット
       this.$store.commit('addTask', {
         name: this.newTaskName,
+        labelIds: this.newTaskLabelIds
       })
       this.newTaskName = ''
+      this.newTaskLabelIds = []
     },
 
     // タスクの完了状態を更新する
@@ -41,6 +70,21 @@ export default {
       this.$store.commit('toggleTaskStatus', {
         id: task.id
       })
+    },
+
+    // ラベルを追加する
+    addLabel () {
+      // addLabel ミューテーションををコミット
+      this.$store.commit('addLabel', {
+        text: this.newLabelText
+      })
+      this.newLabelText = ''
+    },
+
+    // ラベルのIDから、そのラベルのてきすとを返す
+    getLabelText (id) {
+      const label = this.labels.filter(label => label.id === id)[0]
+      return label ? label.text : ''
     },
   }
 }
