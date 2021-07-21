d35450cd01b511b0140707ff86a1f06f0dd4d240
huro3h <huro3h@gmail.com>
2021-07-21 20:36:52 +0900

    add labels state

--- src/store.js
+++ src/store.js
@@ -8,15 +8,40 @@ const store = new Vuex.Store({
   state: {
     // タスクの初期ステート
     tasks: [
-      { id: 1, name: '食う', done: false },
-      { id: 2, name: '寝る', done: false },
-      { id: 3, name: 'あそぶ', done: false },
-    ]
+      {
+        id: 1,
+        name: '食う',
+        labelIds: [1, 2],
+        done: true
+      },
+      {
+        id: 2,
+        name: '寝る',
+        labelIds: [3],
+        done: false
+      },
+      {
+        id: 3,
+        name: 'あそぶ',
+        labelIds: [3, 4],
+        done: false
+      },
+    ],
+    
+    // ラベルの初期ステート
+    labels: [
+      { id: 1, text: '衣' },
+      { id: 2, text: '食' },
+      { id: 3, text: '住' },
+      { id: 4, text: 'その他' }
+    ],
+    
+    nextTaskId: 4,
+    nextLabelId: 5,
   },
-  nextTaskId: 4,
-  
+
   mutations: {
-    addTask (state, { name }) {
+    addTask(state, {name}) {
       state.tasks.push({
         id: state.nextTaskId,
         name,
@@ -27,11 +52,11 @@ const store = new Vuex.Store({
     },
     
     // タスクの完了状態を変更する
-    toggleTaskStatus (state, { id }) {
+    toggleTaskStatus(state, {id}) {
       const filterd = state.tasks.filter(task => {
         return task.id === id
       })
-  
+      
       filterd.forEach(task => {
         task.done = !task.done // ここで値を反転
       })
